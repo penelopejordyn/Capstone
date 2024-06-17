7c23576a421e1b359f948a394117dada828efd9b
penny <pennyjlynette@gmail.com>
2024-06-17 06:10:56 -0800

    cant get logging in to work. registering: object of type json not serializable. login: invalid salt

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session, flash
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from models import db, user, connect_db
 from flask_debugtoolbar import DebugToolbarExtension
 from secret import Authorization
+from forms import RegisterForm, LoginForm
 import requests
 import random
 
@@ -35,35 +36,25 @@ headers = {
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    
     user_ip = request.remote_addr
     paramsip = {"query": user_ip}
     respip = requests.get(ipurl, params=paramsip)
     dataip = respip.json()
     zip_code = dataip['zip']
-    
     location = zip_code
-    
-    
-    
     all_businesses = []
     offset = 0
-    while len(all_businesses) < 200:  # Keep making requests until we have 200 businesses
+    while len(all_businesses) < 500:  # Keep making requests until we have 500 businesses
         params = {"location": location, "limit": 50, "offset": offset}
         resp = requests.get(url, headers=headers, params=params)
         data = resp.json()
         all_businesses.extend(data['businesses'])
         offset += 50
-    
-    
     all_categories = set()
     for business in data['businesses']:
         for category in business['categories']:
             all_categories.add(category['title'])
-            
     selected_categories = random.sample(all_categories, 5)
-    
-   # Select businesses that belong to the selected categories
     category_businesses = {}
     for category in selected_categories:
         category_businesses[category] = []
@@ -75,12 +66,10 @@ def home_page():
                     'zip_code': business['location']['zip_code'],
                     'state': business['location']['state'],
                     'image_url': business['image_url']
-                    # Add more business details here
                 }
                 category_businesses[category].append(business_info)
             if len(category_businesses[category]) >= 20:
                 break  # We have enough businesses for this category
-    
     return render_template('Home_page.html', category_businesses=category_businesses, zip_code=zip_code)
 
 @app.route('/Search_results', methods=['GET', 'POST'])
@@ -100,4 +89,74 @@ def search_results():
     data = resp.json()
     return render_template('Search_results.html',data=data, searchparams=searchparams,location=location)
   
-   
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    """Register user: produce form & handle form submission."""
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        name = form.username.data
+        pwd = form.password.data
+
+        nuser = user.register(name, pwd)
+        db.session.add(nuser)
+        db.session.commit()
+
+        session["user_id"] = user.id
+
+        # on successful login, redirect to secret page
+        return redirect("/")
+
+    else:
+        return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Produce login form or handle login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        name = form.username.data
+        pwd = form.password.data
+
+        # authenticate will return a user or False
+        nuser = user.authenticate(name, pwd)
+
+        if user:
+            session["user_id"] = user.id  # keep logged in
+            return redirect("/secret")
+
+        else:
+            form.username.errors = ["Bad name/password"]
+
+    return render_template("login.html", form=form)
+# end-login
+
+
+@app.route("/secret")
+def secret():
+    """Example hidden page for logged-in users only."""
+
+    if "user_id" not in session:
+        flash("You must be logged in to view!")
+        return redirect("/")
+
+        # alternatively, can return HTTP Unauthorized status:
+        #
+        # from werkzeug.exceptions import Unauthorized
+        # raise Unauthorized()
+
+    else:
+        return render_template("secret.html")
+
+
+@app.route("/logout")
+def logout():
+    """Logs user out and redirects to homepage."""
+
+    session.pop("user_id")
+
+    return redirect("/")
